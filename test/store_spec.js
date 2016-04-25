fa6bc9c839dbe0fcea7109a7ae47b4a478aba576
lfepp <lucasfepp@gmail.com>
2016-04-25 18:32:13 -0400

    Added redux store and unit tests

---
+++ test/store_spec.js
@@ -0,0 +1,33 @@
+import {fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import {getCurrentDate} from '../src/core';
+import makeStore from '../src/store';
+
+describe('store logic', () => {
+
+  describe('makeStore', () => {
+
+    it('ensures the redux store is configured with the reducer', () => {
+      const store = makeStore();
+      expect(store.getState()).to.equal(fromJS({}));
+
+      store.dispatch({ type: 'ADD_DATE' });
+      store.dispatch({ type: 'ADD_IDEA', date: getCurrentDate(), idea: 'Test idea 1' });
+      store.dispatch({ type: 'ADD_IDEA', date: getCurrentDate(), idea: 'Test idea 2' });
+      store.dispatch({ type: 'ADD_IDEA', date: getCurrentDate(), idea: 'Test idea 3' });
+      store.dispatch({ type: 'ADD_IDEA', date: getCurrentDate(), idea: 'Test idea 4' });
+      store.dispatch({ type: 'ADD_IDEA', date: getCurrentDate(), idea: 'Test idea 5' });
+      store.dispatch({ type: 'REMOVE_IDEA', date: '03-18-2016', indexOfIdea: 3 });
+      store.dispatch({ type: 'UPDATE_IDEA', date: '03-18-2016', indexOfIdea: 1, newIdea: 'Updated idea' });
+      expect(store.getState()).to.equal(fromJS({
+        dates: [
+          {
+            date: getCurrentDate(),
+            ideas: ['Test idea 1','Updated idea','Test idea 3','Test idea 5']
+          }
+        ]
+      }))
+    });
+  });
+});
